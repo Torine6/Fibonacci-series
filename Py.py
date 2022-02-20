dc91fc80090409fb403f50f3c5a85df03b6c601f
Torine6 <87073802+Torine6@users.noreply.github.com>
2022-02-20 21:31:53 +0300

    Update Py.py
    Additional comments including the definition.

--- Py.py
+++ Py.py
@@ -1,11 +1,14 @@
+# this is a Python fuction that checks whether a number belongs to the Fibonacci sequence or not
+# in Fibonnacci sequence each number is the sum of the two preciding numbers
+# a number is Fibonacci iff 5n^2+4 or 5n^2-4 is a perfect square
 import math
 
-
+# defining the function and finding square root of the number
 def perfect_square(x):
     num = int(math.sqrt(x))
     return num * num == x
 
-
+# checking whether line 3 is true for the given number
 number = int(input('Enter the number: '))
 result1 = 5 * (number * number) + 4
 result2 = 5 * (number * number) - 4
